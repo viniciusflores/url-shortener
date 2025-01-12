1e15969a7db570e6d5becfefa910bfbd12b390de
Vinicius Flores <viniciusflores379@gmail.com>
2025-01-12 02:50:13 -0300

    :zap: Add rate limit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 import routes from './routes.js';
 import { loggerMiddleware } from './middlewares/loggerMiddleware.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -9,6 +10,15 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    statusCode: 429,
+  }),
+);
 app.use(loggerMiddleware);
 app.use(routes);
 
