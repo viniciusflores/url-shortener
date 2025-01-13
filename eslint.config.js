9e8457f703c853e46700934a2f8e4f3dd392a7e8
Vinicius Flores <viniciusflores379@gmail.com>
2025-01-13 05:44:58 -0300

    :art: Include validation to mocha in e2e tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import mochaPlugin from 'eslint-plugin-mocha';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  mochaPlugin.configs.flat.recommended,
 ];
